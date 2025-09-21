5dd4fc1a26868ba27bd5af71499c2a70ff69c10e
Wulja <158846266+Wuljaa@users.noreply.github.com>
2025-09-21 19:19:38 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Filmovizija
+{
+    
+    public partial class App : Application
+    {
+    }
+}
---
+++ AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Filmovizija.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Filmovizija
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Admin> Admini { get; set; }
+        public DbSet<Korisnik> Korisnici { get; set; }
+        public DbSet<Film> Filmovi { get; set; }
+        public DbSet<Posudba> Posudbe { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=filmovizija.db");
+        }
+
+    }
+}
+
