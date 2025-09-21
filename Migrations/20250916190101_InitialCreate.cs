deb874507603de608490cc6ca8d079c88119d264
Wulja <158846266+Wuljaa@users.noreply.github.com>
2025-09-21 19:14:20 +0200

    Add files via upload

---
+++ Migrations/20250916190101_InitialCreate.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Filmovizija.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admini",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Lozinka = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admini", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Filmovi",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Naziv = table.Column<string>(type: "TEXT", nullable: false),
+                    Zanr = table.Column<string>(type: "TEXT", nullable: false),
+                    GodinaIzdanja = table.Column<int>(type: "INTEGER", nullable: false),
+                    Reziser = table.Column<string>(type: "TEXT", nullable: false),
+                    Ocjena = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmovi", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Korisnici",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Ime = table.Column<string>(type: "TEXT", nullable: false),
+                    Prezime = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Korisnici", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admini");
+
+            migrationBuilder.DropTable(
+                name: "Filmovi");
+
+            migrationBuilder.DropTable(
+                name: "Korisnici");
+        }
+    }
+}
---
+++ Migrations/20250920180819_DodanePosudbe.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Filmovizija.Migrations
+{
+    /// <inheritdoc />
+    public partial class DodanePosudbe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posudbe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    KorisnikId = table.Column<int>(type: "INTEGER", nullable: false),
+                    FilmId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DatumPosudbe = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DatumPovratka = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Vraceno = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posudbe", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posudbe_Filmovi_FilmId",
+                        column: x => x.FilmId,
+                        principalTable: "Filmovi",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Posudbe_Korisnici_KorisnikId",
+                        column: x => x.KorisnikId,
+                        principalTable: "Korisnici",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posudbe_FilmId",
+                table: "Posudbe",
+                column: "FilmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posudbe_KorisnikId",
+                table: "Posudbe",
+                column: "KorisnikId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posudbe");
+        }
+    }
+}
